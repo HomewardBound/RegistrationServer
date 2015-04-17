9e9f1a6cac5078099d8bf8fc856ed973536ad9cc
Brian Broll <brian.broll@vanderbilt.edu>
2015-04-17 13:53:08 -0500

    Added some production settings

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 var Server = require('./Server'),
-    server = new Server();
+    opts = {},
+    server;
 
+// Set config
+if (process.env.NODE_ENV === 'production') {
+    opts.port = 80;
+}
+
+server = new Server(opts);
 server.start();
